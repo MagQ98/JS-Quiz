a25aefab0617de6e7d80922a07c8bcf5290e4358
MagQ98 <maggieq.quinlan@gmail.com>
2021-06-26 18:22:19 +0000

    Rewrite javascript to work with seperate quizzes and fix errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,36 +153,104 @@ let questions2 = [{
     correct: "B"
 }, ];
 
-//Variables
-const lastQuestion = questions.length - 1;
-const lastQuestion2 = questions2.length - 1;
-let runningQuestion = 0;
+//Array to hold questions
+currentQuizQuestions = [
+    {
+        question: "Who created the genophage?",
+        choiceA: "Turians",
+        choiceB: "Protheans",
+        choiceC: "Salarians",
+        choiceD: "Vorcha",
+        correct: "C"
+    }, {
+        question: "What side effects does Kaidan Alenko suffer from because of his L2 Biotic Implants?",
+        choiceA: "Dizziness",
+        choiceB: "Insomnia",
+        choiceC: "Migraines",
+        choiceD: "Tremors",
+        correct: "C"
+    }, {
+        question: "The Drell share which other race’s homeworld?",
+        choiceA: "Salarians",
+        choiceB: "Elcor",
+        choiceC: "Hanar",
+        choiceD: "Asari",
+        correct: "C"
+    }, {
+        question: "On what planet does Shepard first encounter the Asari scientist Liara?",
+        choiceA: "Feros",
+        choiceB: "Therom",
+        choiceC: "Noveria",
+        choiceD: "Tuchanka",
+        correct: "B"
+    }, {
+        question: "Who created the Geth?",
+        choiceA: "Reapers",
+        choiceB: "Quarians",
+        choiceC: "Turians",
+        choiceD: "Drell",
+        correct: "B"
+    }, {
+        question: "What is Samara's daughter, Morinth?",
+        choiceA: "Justicar",
+        choiceB: "Spectre",
+        choiceC: "Ardat Yakshi",
+        choiceD: "Commando",
+        correct: "C"
+    }, {
+        question: "What was the name of the Prothean V.I on Ilos?",
+        choiceA: "Vigil",
+        choiceB: "Glyph",
+        choiceC: "EDI",
+        choiceD: "Avina",
+        correct: "A"
+    }, {
+        question: "Who does Barla Von work for?",
+        choiceA: "Blood Pack",
+        choiceB: "Shadow Broker",
+        choiceC: "Eclipse",
+        choiceD: "The Consort",
+        correct: "B"
+    }, {
+        question: "Which of these is not the name of a nightclub found across the Mass Effect universe?",
+        choiceA: "Flux",
+        choiceB: "Purgatory",
+        choiceC: "Dark Star",
+        choiceD: "Inferno",
+        correct: "D"
+    }, {
+        question: "The Protheans are deities to the Hanar, what is their religious name?",
+        choiceA: "The Collectors",
+        choiceB: "The Enkindlers",
+        choiceC: "The Old Gods",
+        choiceD: "The Inspirits",
+        correct: "B"
+    }, 
+]
 
+//Variables
+let runningQuestion = 0;
+let lastQuestion = 0;
 //Show Question
 function renderQuestion() {
-    let q = questions[runningQuestion];
+    let q = currentQuizQuestions[runningQuestion];
     question.innerHTML = "<p>" + q.question + "</p>";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
     choiceD.innerHTML = q.choiceD;
-}
-
-function renderQuestion2() {
-    let q2 = questions2[runningQuestion];
-    question.innerHTML = "<p>" + q2.question + "</p>";
-    choiceA.innerHTML = q2.choiceA;
-    choiceB.innerHTML = q2.choiceB;
-    choiceC.innerHTML = q2.choiceC;
-    choiceD.innerHTML = q2.choiceD;
-}
+};
 
 //Start button to begin quiz
 start.onclick = function () {
+    currentQuizQuestions = questions
+    lastQuestion = currentQuizQuestions.length - 1;
     startQuiz();
 };
 start2.onclick = function () {
-    startQuiz2();
+    currentQuizQuestions = questions2
+    lastQuestion = currentQuizQuestions.length - 1;
+    startQuiz();
 };
 
 //Remove start button and show quiz section with questions
@@ -190,14 +258,6 @@ function startQuiz() {
     start.style.display = "none";
     start2.style.display = "none";
     renderQuestion();
-    quiz.style.display = "block";
-    renderProgress();
-}
-
-function startQuiz2() {
-    start.style.display = "none";
-    start2.style.display = "none";
-    renderQuestion2();
     quiz.style.display = "block";
     renderProgress();
 }
@@ -210,7 +270,7 @@ function renderProgress() {
 
 //Checks answer and loops next question until end
 function checkAnswer(answer) {
-    if (answer == questions[runningQuestion].correct) {
+    if (answer == currentQuizQuestions[runningQuestion].correct) {
         answerIsCorrect();
     } else {
         answerIsWrong();
@@ -223,21 +283,7 @@ function checkAnswer(answer) {
     }
 }
 
-function checkAnswer2(answer) {
-    if (answer == questions2[runningQuestion].correct) {
-        answerIsCorrect();
-    } else {
-        answerIsWrong();
-    }
-    if (runningQuestion < lastQuestion2) {
-        runningQuestion++;
-        renderQuestion2();
-    } else {
-        return window.location.assign('/end.html');
-    }
-}
-
-/*Changes progress bar color if correct or incorrect*/
+//Changes progress bar color if correct or incorrect
 function answerIsCorrect() {
     document.getElementById(runningQuestion).style.backgroundColor = "rgb(14,185,254)";
     document.getElementById(runningQuestion).innerHTML = '<i class="fas fa-check"></i>';
